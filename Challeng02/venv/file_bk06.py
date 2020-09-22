dae7a3e0d659ea00902ff2dcc47ecca0d5789191
scottbing <sbing52@comcast.net>
2020-09-21 22:39:30 -0600

    sb_5410_midterm_challenge
    sb_5410_midterm_challenge

---
+++ Challeng02/venv/file_bk06.py
@@ -0,0 +1,66 @@
+import string
+import re
+
+def process_file(fname, enc):
+    #open file for 'r'eading
+    with open(fname, 'r', encoding=enc) as file:
+        dat = file.read()   #read file
+        dat = perform_re(dat)
+    return(dat.split('\n'))  #return read data
+#end process_file(fname, enc):
+
+def write_results(fname, data, enc):
+    #open file for 'w'riting
+    with open(fname, 'w', encoding = enc) as file:
+        file.write(data)
+#end def write_results(fname, data, enc):
+
+def words_to_dict(all_words, dictionary):
+    for w in all_words: #for each word
+        w = clean_word((w))
+        if w in dictionary:  #if the word was counted before
+            dictionary[w] += 1  #increment te count
+        else:
+            dictionary[w] = 1   #begin count for a new word
+#end def word_to_dict(all_words, dictionary):
+
+def clean_word(word):
+    for p in string.punctuation:
+        word = word.replace(p, "")
+    return word.lower() #return the word as lowercase
+#end def clean_word(word):
+
+def perform_re(text):
+    text = re.sub(r'\d+\.(.+)', '\\1', text)
+    #text = re.sub("\([A-Z].*?\)","", text)
+    text = re.sub("\([A-Z0-9.'\s]+\)", "", text)
+    return text
+
+def main():
+    unique_words = {}   #dictionary for word counts
+    words = process_file("songs.txt", "utf-8")
+    words_to_dict(words, unique_words)
+
+
+    print("Found {0} total words.".format(len(words)))
+    print("Found {0} unique words.".format(len(unique_words.keys())))
+    # print("Here are a few: ")
+    # print(list(unique_words.keys())[:5])    #print first few unique words
+    # result = unique_words.get('python', 0)
+    # print("Python appears {0} times in the text.".format(result))
+
+    # srch_str = 'down'
+    # if srch_str in unique_words.keys():
+    #     print("down appears {0} times in the text.".format(unique_words[srch_str]))
+    # else:
+    #     print(srch_str, "not in text.")
+
+    print("Raw TTR: ", len(unique_words.keys()) / len(words))
+    print("Pct TTR: ", str(round((len(unique_words.keys()) / len(words))*100)) + "%")
+
+    #join the words in the list with new line char
+    write_results("perline.txt", "\n".join(words), "utf-8")
+#end of main():
+
+if __name__ == "__main__":
+    main()
